964e92a73cfc30743029e368b474780b0e93a79c
himani1998 <himanibansal1998@gmail.com>
2021-01-12 11:14:14 -0500

    Initial Commit

---
+++ src/main/java/himani/joke/app/Jokeapplicationspring/JokeApplicationSpringApplication.java
@@ -0,0 +1,13 @@
+package himani.joke.app.Jokeapplicationspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeApplicationSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokeApplicationSpringApplication.class, args);
+	}
+
+}
